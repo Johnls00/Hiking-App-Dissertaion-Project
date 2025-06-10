01287a05a525adec1b89dec0bdc0b21ed7690cf0
John Livingstone <jlivingstone08@qub.ac.uk>
2025-06-10 19:07:17 +0100

    trail view temp screen

---
+++ hiking_app/lib/screens/trail_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+class TrailViewScreen extends StatefulWidget {
+  const TrailViewScreen({super.key});
+
+  @override
+  State<TrailViewScreen> createState() => _TrailViewScreenState();
+}
+
+class _TrailViewScreenState extends State<TrailViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(241, 244, 248, 1),
+
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
