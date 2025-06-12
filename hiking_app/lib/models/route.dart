5d6a2080239398d10bd20703edcb9dae0b1e7420
John Livingstone <jlivingstone08@qub.ac.uk>
2025-06-12 23:18:58 +0100

    simplified route classes

--- hiking_app/lib/models/route.dart
+++ hiking_app/lib/models/route.dart
@@ -1,15 +1,17 @@
+import 'package:hiking_app/models/waypoint.dart';
+
 /// an abstract class which represents a route
 /// a route must have a name and a list of waypoints which link to make the route
 class TrailRoute {
-  final String name;
-  final String routeFile;
-  final String location;
-  final Duration timeToComplete;
-  final double distance;
-  final String difficulty;
-  final String description;
-  final List<String> images;
-  final List<String> waypoints;
+  String name;
+  String routeFile;
+  String location;
+  Duration timeToComplete;
+  double distance;
+  String difficulty;
+  String description;
+  List<String> images;
+  List<Waypoint> waypoints;
 
   TrailRoute(
     this.name,
@@ -22,6 +24,7 @@ class TrailRoute {
     this.images,
     this.waypoints,
   );
+
 
   void printSummary(){
     print('name: $name, location: $location, distance: $distance');
