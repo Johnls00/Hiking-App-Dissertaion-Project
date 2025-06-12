e9ec9c470a670c7fa1a4f64c38305c9d2ec653f9
John Livingstone <jlivingstone08@qub.ac.uk>
2025-06-12 18:09:48 +0100

    route class updated

--- hiking_app/lib/models/route.dart
+++ hiking_app/lib/models/route.dart
@@ -1,13 +1,29 @@
 /// an abstract class which represents a route
 /// a route must have a name and a list of waypoints which link to make the route
-abstract class Route {
+class TrailRoute {
   final String name;
   final String routeFile;
+  final String location;
+  final Duration timeToComplete;
+  final double distance;
+  final String difficulty;
+  final String description;
+  final List<String> images;
+  final List<String> waypoints;
 
-  Route({
-    required this.name,
-    required this.routeFile,
-  });
+  TrailRoute(
+    this.name,
+    this.routeFile,
+    this.location,
+    this.timeToComplete,
+    this.distance,
+    this.difficulty,
+    this.description,
+    this.images,
+    this.waypoints,
+  );
 
-  void printSummary(); //abstract method
+  void printSummary(){
+    print('name: $name, location: $location, distance: $distance');
+  } //abstract method
 }
