f99754f0a821eab9627843c83e4560ba0108e776
John Livingstone <jlivingstone08@qub.ac.uk>
2025-06-14 17:35:38 +0100

    class which handles reading gpx files and mapping contents to required format

--- hiking_app/lib/utilities/gpx_file_util.dart
+++ hiking_app/lib/utilities/gpx_file_util.dart
@@ -2,26 +2,32 @@ import 'package:flutter/services.dart' show rootBundle;
 import 'package:gpx/gpx.dart';
 import 'package:hiking_app/models/waypoint.dart';
 
-readGpxFile(String filePath) async {
-  final xmlString = await rootBundle.loadString(filePath);
-  final gpxFile = GpxReader().fromString(xmlString);
+class GpxFileUtil {
 
-  return gpxFile;
-}
+  static Future<Gpx> readGpxFile(String filePath) async {
+    String xmlString = await rootBundle.loadString(filePath);
+    Gpx gpxFile = GpxReader().fromString(xmlString);
 
-// method which maps the waypoints to a waypoint object and returns them as a list
-List<Waypoint> mapWaypoints(final gpxFile) {
-  final List<Waypoint> mappedWaypoints = [];
-
-  for (var wpt in gpxFile.wpts) {
-    double lon = wpt.lon;
-    double lat = wpt.lat;
-    double ele = wpt.ele;
-    String waypointName = wpt.name;
-    // add the waypoints to the list of waypoints to be updated in the route class
-    mappedWaypoints.add(Waypoint(name: waypointName, lat: lat, lon: lon, ele: ele));
+    return gpxFile;
   }
 
-  return mappedWaypoints;
-}
+  // method which maps the waypoints to a waypoint object and returns them as a list
+  static List<Waypoint> mapWaypoints(Gpx gpxFile) {
+    List<Waypoint> mappedWaypoints = [];
 
+    for (var wpt in gpxFile.wpts) {
+      double? lon = wpt.lon;
+      double? lat = wpt.lat;
+      double? ele = wpt.ele;
+      String? waypointName = wpt.name;
+
+      if (lon != null && lat != null && ele != null && waypointName != null) {
+        mappedWaypoints.add(
+          Waypoint(name: waypointName, lat: lat, lon: lon, ele: ele),
+        );
+      }
+    }
+
+    return mappedWaypoints;
+  }
+}
