e1d4626a448bdf933b5832deff68af2ca5678023
John Livingstone <jlivingstone08@qub.ac.uk>
2025-06-21 21:05:38 +0100

    trail card design changed image takes full width

--- hiking_app/lib/widgets/trail_card.dart
+++ hiking_app/lib/widgets/trail_card.dart
@@ -1,14 +1,11 @@
 // lib/widgets/trails_card.dart
 import 'package:flutter/material.dart';
 import 'package:hiking_app/models/route.dart';
-import 'package:hiking_app/screens/trail_view.dart';
 
 class TrailCard extends StatelessWidget {
   final TrailRoute trailRoute;
-  
-  const TrailCard({super.key, 
-    required this.trailRoute,
-  });
+
+  const TrailCard({super.key, required this.trailRoute});
 
   @override
   Widget build(BuildContext context) {
@@ -16,56 +13,40 @@ class TrailCard extends StatelessWidget {
       width: double.infinity,
       child: InkWell(
         onTap: () {
-          Navigator.pushNamed(
-            context,
-            '/trail_view',
-            arguments: trailRoute,
-          );
+          Navigator.pushNamed(context, '/trail_view', arguments: trailRoute);
         },
         borderRadius: BorderRadius.circular(12),
         child: Card(
-          elevation: 1,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
           margin: EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // trail image
               ClipRRect(
                 borderRadius: BorderRadius.circular(12),
                 child: Image.asset(
                   trailRoute.images.first,
-                  width: 150, // fixed width for the image
+                  width: double.maxFinite, // fixed width for the image
                   height: 150,
                   fit: BoxFit.cover,
                 ),
               ),
-              SizedBox(width: 16),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // trail name text
-                    Text(
-                      trailRoute.name,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    //location text
-                    Text(
-                      trailRoute.location,
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    // trail description
-                    Text(trailRoute.description),
-                  ],
-                ),
+              // trail name text
+              Text(
+                trailRoute.name,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
+              //location text
+              Text(
+                trailRoute.location,
+                style: TextStyle(fontSize: 12, fontWeight: FontWeight.normal),
+              ),
+              SizedBox(height: 8),
+              // trail description
+              Text(trailRoute.description),
             ],
           ),
         ),
