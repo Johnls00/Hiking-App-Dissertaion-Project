aa0756a19ed1fb85eafd78080727de0311046e91
John Livingstone <jlivingstone08@qub.ac.uk>
2025-06-25 14:29:07 +0100

    added styling to trail overview page

--- hiking_app/lib/screens/trail_view.dart
+++ hiking_app/lib/screens/trail_view.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:hiking_app/models/route.dart';
+import 'package:hiking_app/screens/trail_waypoints_view.dart';
+import 'package:hiking_app/widgets/round_back_button.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
 class TrailViewScreen extends StatefulWidget {
   const TrailViewScreen({super.key});
@@ -8,9 +14,37 @@ class TrailViewScreen extends StatefulWidget {
   State<TrailViewScreen> createState() => _TrailViewScreenState();
 }
 
-int waypointIndex = 0;
+late MapboxMap mapboxMap;
 
 class _TrailViewScreenState extends State<TrailViewScreen> {
+  void _onMapCreated(MapboxMap controller) async {
+    mapboxMap = controller;
+    final trailRoute = ModalRoute.of(context)!.settings.arguments as TrailRoute;
+
+    final lineString = LineString(
+      coordinates: trailRoute.trackpoints.map((p) => p.coordinates).toList(),
+    );
+    final feature = Feature(geometry: lineString, id: "route_line");
+    final featureCollection = FeatureCollection(features: [feature]);
+
+    await mapboxMap.style.addSource(
+      GeoJsonSource(id: "line", data: jsonEncode(featureCollection.toJson())),
+    );
+
+    await mapboxMap.style.addLayer(
+      LineLayer(
+        slot: "middle",
+        id: "line_layer_trail_overview",
+        sourceId: "line",
+        lineColor: Colors.red.toARGB32(),
+        lineBorderColor: Colors.red.shade900.toARGB32(),
+        lineJoin: LineJoin.ROUND,
+        lineCap: LineCap.ROUND,
+        lineWidth: 6.0,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final trailRoute = ModalRoute.of(context)!.settings.arguments as TrailRoute;
@@ -18,55 +52,286 @@ class _TrailViewScreenState extends State<TrailViewScreen> {
     return Scaffold(
       backgroundColor: Color.fromRGBO(241, 244, 248, 1),
 
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(trailRoute.name, textAlign: TextAlign.center),
-
-          Image.asset(
-            trailRoute.images.first,
-            width: double.infinity,
-            height: 400,
-            fit: BoxFit.cover,
-          ),
-
-          SizedBox(height: 8),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              IconButton(
-                onPressed: () {
-                  setState(() {
-                    if (waypointIndex > 0) {
-                      waypointIndex--;
-                    }
-                  });
-                },
-                icon: const Icon(Icons.arrow_back),
+              RoundBackButton(),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(
+                    trailRoute.images.first,
+                    width: double.maxFinite, // fixed width for the image
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
 
-              Column(
+              SizedBox(height: 8),
+
+              Row(
                 children: [
-                  Text(trailRoute.waypoints[waypointIndex].name),
-                  Text(trailRoute.waypoints[waypointIndex].returnCoordinates()),
+                  SizedBox(width: 10),
+                  Text(trailRoute.name, style: TextStyle(fontSize: 24)),
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(width: 10),
+                  Text(
+                    trailRoute.location,
+                    style: TextStyle(fontSize: 12, color: Colors.black54),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(width: 10),
+                  Text(
+                    "Rating",
+                    style: TextStyle(fontSize: 12, color: Colors.black54),
+                  ),
+                ],
+              ),
+              Divider(color: Colors.black, indent: 10, endIndent: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            (trailRoute.distance / 1000).toStringAsFixed(2),
+                            style: TextStyle(fontSize: 32, color: Colors.black),
+                          ),
+                          Text(
+                            "km",
+                            style: TextStyle(color: Colors.black, fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "Distance",
+                        style: TextStyle(color: Colors.black45, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            (trailRoute.elevation).toStringAsFixed(2),
+                            style: TextStyle(fontSize: 32, color: Colors.black),
+                          ),
+                          Text(
+                            "m",
+                            style: TextStyle(color: Colors.black, fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "Elevation",
+                        style: TextStyle(color: Colors.black45, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            ((trailRoute.distance) * 5.1 / 1000)
+                                .toStringAsFixed(2),
+                            style: TextStyle(fontSize: 32, color: Colors.black),
+                          ),
+                          Text(
+                            "mins",
+                            style: TextStyle(color: Colors.black, fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "Average time",
+                        style: TextStyle(color: Colors.black45, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Divider(color: Colors.black, indent: 10, endIndent: 10),
+              Row(
+                children: [
+                  SizedBox(width: 10),
+                  Text(
+                    trailRoute.description,
+                    style: TextStyle(color: Colors.black, fontSize: 13),
+                  ),
+                ],
+              ),
+              Divider(color: Colors.black, indent: 10, endIndent: 10),
+              Row(
+                children: [
+                  SizedBox(width: 10),
+                  Text(
+                    "Trail Overview",
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
                 ],
               ),
 
-              IconButton(
-                onPressed: () {
-                  setState(() {
-                    if (waypointIndex < (trailRoute.waypoints.length - 1)) {
-                      waypointIndex++;
-                    }
-                  });
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: double.maxFinite,
+                  height: 250,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: MapWidget(
+                    key: ValueKey(
+                      waypointIndex,
+                    ), // rebuilds map when index changes
+                    onMapCreated: _onMapCreated,
+                    cameraOptions: CameraOptions(
+                      center: trailRoute.trackpoints.first,
+                      zoom: 15.5,
+                    ),
+                  ),
+                ),
+              ),
+              Divider(color: Colors.black, indent: 10, endIndent: 10),
+
+              InkWell(
+                onTap: () {
+                  Navigator.pushNamed(
+                    context,
+                    '/trail_waypoints_view',
+                    arguments: trailRoute,
+                  );
                 },
-                icon: const Icon(Icons.arrow_forward),
+                child: SizedBox(
+                  height: 49,
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      SizedBox(width: 15),
+                      Ink(
+                        width: 49,
+                        height: 49,
+                        decoration: const ShapeDecoration(
+                          color: Color.fromRGBO(221, 221, 221, 1),
+                          shape: CircleBorder(),
+                        ),
+                        child: Icon(Icons.flag_outlined),
+                      ),
+                      const SizedBox(width: 10), // spacing between icon and text
+                      Expanded(
+                        child: Text(
+                          "Explore points of interest",
+                          style: TextStyle(fontSize: 13),
+                        ),
+                      ),
+                      Icon(size: 30, Icons.arrow_circle_right_outlined),
+                      const SizedBox(width: 20), // optional right padding
+                    ],
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 12),
+
+              InkWell(
+                onTap: () {
+                  Navigator.pushNamed(
+                    context,
+                    '/trail_waypoints_view',
+                    arguments: trailRoute,
+                  );
+                },
+                child: SizedBox(
+                  height: 49,
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      SizedBox(width: 15),
+                      Ink(
+                        width: 49,
+                        height: 49,
+                        decoration: const ShapeDecoration(
+                          color: Color.fromRGBO(221, 221, 221, 1),
+                          shape: CircleBorder(),
+                        ),
+                        child: Icon(Icons.rate_review_outlined),
+                      ),
+                      const SizedBox(width: 10), // spacing between icon and text
+                      Expanded(
+                        child: Text(
+                          "Read reviews",
+                          style: TextStyle(fontSize: 13),
+                        ),
+                      ),
+                      Icon(size: 30, Icons.arrow_circle_right_outlined),
+                      const SizedBox(width: 20), // optional right padding
+                    ],
+                  ),
+                ),
+              ),
+
+              Divider(color: Colors.black, indent: 10, endIndent: 10),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 166,
+                    height: 42,
+                    child: Ink(
+                      decoration: ShapeDecoration(
+                        color: Color.fromRGBO(221, 221, 221, 1),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          TextButton(onPressed: () {}, child: Text("Download", style: TextStyle(fontSize: 13, color: Colors.black),)),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 166,
+                    height: 42,
+                    child: Ink(
+                      decoration: ShapeDecoration(
+                        color: Color.fromRGBO(221, 221, 221, 1),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          TextButton(onPressed: () {}, child: Text("View Map", style: TextStyle(fontSize: 13, color: Colors.black),)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
